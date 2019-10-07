0721f7dca5ad8c8f59a9df93a2f186c3206fa75d
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-10-07 06:25:53 +0000

    init, employee table component

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,30 @@
+<template>
+<table>
+  <thead>
+    <tr>
+      <th>Employee Name</th>
+      <th>Employee Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Kofi Ramos</td>
+      <td>kofiramos@gmail.com</td>
+    </tr>
+    <tr>
+      <td>Kojo Ramos</td>
+      <td>kojoramos@gmail.com</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<script>
+export default {
+  name: 'employee-table',
+}
+</script>
+
+<style scoped>
+
+</style>
